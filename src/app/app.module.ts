470e1b1cb4472bb6e67280a1f8d53dc1f390a17d
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-06 20:13:24 -0300

    trabalhando com eventos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentComponent } from './components/parent/parent.component';
 import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventsComponent } from './components/events/events.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
     ParentComponent,
-    DirectivesComponent
+    DirectivesComponent,
+    IfRenderComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
